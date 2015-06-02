02118567dcb1cbba59083b0dd1c9504d27d14423
Robbie Minshall <rjminsha@us.ibm.com>
2015-06-01 21:39:14 -0400

    adding script to pull en source files

---
+++ pull_pipeline_source.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#********************************************************************************
+# Copyright 2014 IBM
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#********************************************************************************
+echo "cloning extension repositories"
+if [ -d update_service ]; then cd update_service; git pull; else git clone  --depth 1 https://github.com/Osthanes/update_service.git;fi 
+if [ -d appscan_dynamic_analyzer ]; then cd appscan_dynamic_analyzer; git pull; else git clone  --depth 1  https://github.com/Osthanes/appscan_dynamic_analyzer.git; fi
+if [ -d notifications ]; then cd notifications; git pull; else git clone  --depth 1 https://github.com/Osthanes/notifications.git ; fi
+if [ -d globalization ]; then cd globalization; git pull; else git clone  --depth 1 https://github.com/Osthanes/globalization.git ; fi
+if [ -d appscan_static_analyzer ]; then cd appscan_static_analyzer; git pull; else git clone  --depth 1 https://github.com/Osthanes/appscan_static_analyzer.git ; fi
+if [ -d docker_builder ]; then cd docker_builder; git pull; else git clone  --depth 1 https://github.com/Osthanes/docker_builder.git ; fi 
+if [ -d container_deployer ]; then cd container_deployer; git pull; else git clone  --depth 1 https://github.com/Osthanes/container_deployer ; fi
+
+echo "packaging up en source files to: osthanes_en_source_files.tar"
+find . -name "Language_en.properties"  | tar -cf osthanes_en_source_files.tar -T -
+tar --list --file osthanes_en_source_files.tar 
+
+
+
+
+
