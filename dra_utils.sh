fd2368c3d2b4116025a0597d579831cf44d113ca
Bahram Sanaei <sanaei@us.ibm.com>
2015-10-15 14:10:47 -0400

    fix slack notify message

--- dra_utils.sh
+++ dra_utils.sh
@@ -375,13 +375,16 @@ dra_grunt_decision(){
         RC=$?
         if [ $RC -eq 0 ]; then
             export DRA_DECISION=$(echo $RESPONSE | sed 's/.*"decision":"//' | awk -F "\"" '{print $1}')
-            export DRA_REPORT_URL=$(echo $RESPONSE | sed 's/.*Check the report at -//' | awk -F "\"" '{print $1}')
+            export DRA_REPORT_URL=$(echo $RESPONSE | sed 's/.*Check the report at -//' | awk -F "[" '{print $1}')
             if [ -n "$DRA_DECISION" ]; then
                 if [ "$DRA_DECISION" == "Proceed" ]; then
+                    ${EXT_DIR}/utilities/sendMessage.sh -l good -m "<${DRA_REPORT_URL}|Deployment Risk Analytics Decision Report> with decision: ${DRA_DECISION}."
                     return 0
                 elif [ "$DRA_DECISION" == "Stop - Advisory" ]; then
+                    ${EXT_DIR}/utilities/sendMessage.sh -l good -m "<${DRA_REPORT_URL}|Deployment Risk Analytics Decision Report> with decision: ${DRA_DECISION}."
                     return 1
                 elif [ "$DRA_DECISION" == "Stop" ]; then
+                    ${EXT_DIR}/utilities/sendMessage.sh -l bad -m "<${DRA_REPORT_URL}|Deployment Risk Analytics Decision Report> with decision: ${DRA_DECISION}."
                     return 2
                 else
                     debugme echo -e "Failed to get correct decision result. The DRA_DECISION is ${DRA_DECISION}"
